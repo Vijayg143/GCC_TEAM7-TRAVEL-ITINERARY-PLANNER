393f4f2e1b64b053123722415cf7a92f7da0a120
VenkatSatyaSaiABHISHEK <145760380+VenkatSatyaSaiABHISHEK@users.noreply.github.com>
2024-02-24 12:23:45 +0530

    Add files via upload

---
+++ flask.js
@@ -0,0 +1,11 @@
+
+document.getElementById("to")
+.addEventListener("click",function(){
+    document.getElementById("red").style.display="flex"
+})
+
+document.getElementById("fg")
+.addEventListener("click",function(){
+    document.getElementById("red").style.display="none"
+})
+  
